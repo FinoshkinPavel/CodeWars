625bb7426e472625f83ce88a3351f6589790ab2c
FinoshkinPavel <100122691+FinoshkinPavel@users.noreply.github.com>
2022-03-27 13:07:40 +0600

    Разобрать хороший int из проблемы char
    Спросите маленькую девочку - "Сколько тебе лет?". Она всегда говорит странные вещи... Давайте поможем ей!
    
    Для правильного ответа программа должна вернуть int от 0 до 9.
    
    Предположим, что тестовая входная строка всегда действительна и может выглядеть как «1 год» или «5 лет» и т. д. Первый символ — это только число.

---
+++ script4.js
@@ -0,0 +1,6 @@
+function getAge(inputString) {
+    // return correct age (int). Happy coding :)
+    return Number(inputString[0]);
+}
+console.log(getAge("4 years old"))
+//getAge("4 years old"), 4
