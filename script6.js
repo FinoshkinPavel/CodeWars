02a1ffd4761588e49969afea17756f576882352d
FinoshkinPavel <100122691+FinoshkinPavel@users.noreply.github.com>
2022-03-28 14:02:22 +0600

    Если не можешь уснуть, считай овец!!
    Задача:
    Например, если задано неотрицательное целое число, 3верните строку с бормотанием: "1 sheep...2 sheep...3 sheep...". Ввод всегда будет действительным, т.е. отрицательных целых чисел не будет.

---
+++ script6.js
@@ -0,0 +1,10 @@
+function countSheep(num) {
+  for(let i = 0; i < num;i++){
+      console.log(`${i} sheep...`);
+    }
+}
+
+countSheep(454); 
+//countSheep(1), "1 sheep..."
+//countSheep(2), "1 sheep...2 sheep..."
+//countSheep(3), "1 sheep...2 sheep...3 sheep..."
