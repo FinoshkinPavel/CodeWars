91781e1ef9021e384eaea82ad038f79feb94b038
FinoshkinPavel <100122691+FinoshkinPavel@users.noreply.github.com>
2022-03-28 14:13:20 +0600

    Напишите функцию, которая разбивает строку и преобразует ее в массив слов.
    Примеры (ввод -> вывод):
    * "Robin Singh" ==> ["Robin", "Singh"]
    
    * "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]

---
+++ script7.js
@@ -0,0 +1,8 @@
+function stringToArray(string) {
+    return string.split(' ')    
+}
+
+console.log(stringToArray("Robin Singh"))
+
+//stringToArray("Robin Singh"), ["Robin", "Singh"]
+//stringToArray("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"]
