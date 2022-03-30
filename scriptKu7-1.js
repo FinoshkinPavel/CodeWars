aa494ff44aee4e36c361d23f4541ae6b65bd285a
FinoshkinPavel <100122691+FinoshkinPavel@users.noreply.github.com>
2022-03-30 16:06:30 +0600

    Numerical Palindrome #1

--- scriptKu7-1.js
+++ scriptKu7-1.js
@@ -1,13 +1,8 @@
 function palindrome(num) {
-    let a = String(num).split('').reverse().join('');
-    if (num < 0) return "Not valid";
-    if (typeof (num) !== Number ) return "Not valid";
-    if (Number(a) !== num) return false;
-    if (Number(a) === num) return true;
-  
+    if(num < 0 || typeof(num) !== 'number') return 'Not valid';
+    let a = String(num).split('').reverse().join(''); 
+     return a == num
 }
-
-//console.log(1221< 0)
 
 console.log(palindrome(1221));//,true
 console.log(palindrome(123322));//,false
@@ -15,17 +10,5 @@ console.log(palindrome("ACCDDCCA"));//,"Not valid"
 console.log(palindrome("1221"));//,"Not valid"
 console.log(palindrome(-450));//,"Not valid"
 
-let num = "ACCDDCCA";
-console.log(typeof(num))
-if(typeof(num) !== Number){
-    console.log('ya')
-} else {
-    console.log('no')
-}
+console.log(typeof(123))
 
-
-
-let c ='asfasf';
-//console.log(c == String);
-
-//пока не решил 
