d778fdcd64919d1f130e3c90a52965463c90fe79
FinoshkinPavel <100122691+FinoshkinPavel@users.noreply.github.com>
2022-03-30 17:07:25 +0600

    кю7-2 не решено

--- scriptKu7-2.js
+++ scriptKu7-2.js
@@ -1,9 +1,18 @@
 function lastIndexOf(head, value) {
-    return -1;
+    let idd = this.head.lastIndexOf(value);
+    return  idd
 }
 
-console.log(listFromArray([1, 2, 3]), 2)//, 1
-//listFromArray(['aaa', 'b', 'abc']), 'aaa'), 0
-//listFromArray([17, '17', 1.2]), 17), 0
-//lastIndexOf(listFromArray([17, '17', 1.2]), '17'), 1
-//lastIndexOf(listFromArray([1, 2, 3, 3]), 3), 3
+//console.log(lastIndexOf(null, 17));
+//dconsole.log(lastIndexOf(listFromArray([1, 2, 3]), 2)) // 1
+// console.log(lastIndexOf(listFromArray(['aaa', 'b', 'abc']), 'aaa')) // 0
+// console.log(lastIndexOf(listFromArray([17, '17', 1.2]), 17)) // 0
+//console.log(lastIndexOf(listFromArray([17, '17', 1.2]), '17'))// 1
+// console.log(lastIndexOf(listFromArray([1, 2, 3, 3]), 3))// 3
+
+
+function arr(a,b){
+    console.log(typeof(a))
+}
+
+arr(listArr([1, 2, 3]), 2)
